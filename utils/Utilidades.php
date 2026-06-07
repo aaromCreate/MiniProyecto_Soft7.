f3830c40d3f61588fc9f350b850669a54499e608
aaromCreate <aarom.ortiz4@gmail.com>
2026-06-07 07:56:35 -0500

    cambio normal

--- utils/Utilidades.php
+++ utils/Utilidades.php
@@ -21,24 +21,4 @@ class Utilidades
     {
         return filter_var($numero, FILTER_VALIDATE_FLOAT);
     }
-
-    public static function calcularPromedio($numeros)
-    {
-        return array_sum($numeros) / count($numeros);
-    }
-
-    public static function desviacionEstandar($numeros)
-    {
-        $promedio = self::calcularPromedio($numeros);
-
-        $suma = 0;
-
-        foreach ($numeros as $numero) {
-
-            $suma += pow($numero - $promedio, 2);
-
-        }
-
-        return sqrt($suma / count($numeros));
-    }
 }
